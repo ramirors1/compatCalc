21bf72a1695b344bfd775e3b937b67861017be2e
Your Github User Name <ramirezr@protonmail.com>
2022-02-28 14:32:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+prompt("What is your name?");
+prompt("What is their name?")
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+if (n < 70) {
+    alert("Your score was " + n + "%, you should keep on looking");
+}
+else { 
+    alert("Your score was " + n + "%, you should go for it.")    
+}
